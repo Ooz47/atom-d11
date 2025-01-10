9ffb332baeb1d0845b6ad5678caf02a4574a0544
Ooz47 <yannick.abaul@gmail.com>
2025-01-09 22:10:22 -0400

    WIP : Upgrade D11
    Installation modules, importations contenus et médias, création custom theme

---
+++ web/themes/custom/atomeco/js/atomeco.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * atomeco behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.atomeco = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
