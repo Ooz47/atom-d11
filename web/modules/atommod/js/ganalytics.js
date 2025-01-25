835c85d99db13d86bdbf3b6fbae5e0cb63578b3a
Ooz47 <yannick.abaul@gmail.com>
2025-01-24 23:16:58 -0400

    Done: MEF admin + Analytics

---
+++ web/modules/atommod/js/ganalytics.js
@@ -0,0 +1,12 @@
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-6998KNPFNM"></script>
+var imported = document.createElement('script');
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=G-6998KNPFNM';
+document.head.appendChild(imported);
+// {/* <script> */}
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6998KNPFNM');
+// {/* </script> */}
